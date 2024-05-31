e561954bd71ea4f5bb5cb569541d650513e406fb
pedro.hernandez <pedro.hernandez@tecsup.edu.pe>
2024-05-31 04:21:27 +0000

    practica lab

---
+++ src/app/components/pagina5/pagina5.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina5',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina5.component.html',
+  styleUrl: './pagina5.component.css'
+})
+export class Pagina5Component {
+
+}
